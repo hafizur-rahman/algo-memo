d336e5c2ec585513de7e02fb9d076814b9bf22e6
hafizur-rahman <bibagimon@gmail.com>
2014-03-17 23:48:03 +0900

    Sorting by muliple key and random shuffle

---
+++ algorithms/src/main/java/com/jdreamer/algo/AlbumShuffle.java
@@ -0,0 +1,72 @@
+package com.jdreamer.algo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class AlbumShuffle {
+
+	private List<Song> shuffleByAlbum(List<Song> songs) {
+		List<Song> shuffled = new ArrayList<Song>();
+		final Map<String, Double> albums = new HashMap<String, Double>();
+
+		Random r = new Random(System.currentTimeMillis());
+		for (Song s : songs) {
+			if (!albums.containsKey(s.album)) {
+				albums.put(s.album, r.nextDouble());
+			}
+
+			s.setRandomRank(r.nextDouble());
+		}
+
+		Collections.sort(songs, new Comparator<Song>() {
+
+			@Override
+			public int compare(Song s1, Song s2) {
+				int c1 = albums.get(s1.album).compareTo(albums.get(s2.album));
+				return c1 == 0 ? Double.compare(s1.randomRank, s2.randomRank)
+						: c1;
+			}
+		});
+
+		return songs;
+	}
+
+	public static void main(String[] args) {
+		List<Song> songs = new ArrayList<Song>();
+		songs.add(new Song("A1", "S1", "T1", 1));
+		songs.add(new Song("A1", "S1", "T2", 2));
+		songs.add(new Song("A1", "S2", "T3", 3));
+		songs.add(new Song("A2", "S1", "T3", 4));
+		songs.add(new Song("A3", "S2", "T4", 5));
+		songs.add(new Song("A3", "S1", "T5", 6));
+
+		List<Song> shuffled = new AlbumShuffle().shuffleByAlbum(songs);
+		for (Song s : shuffled) {
+			System.out.println(s.album + " " + s.title + " " + s.track);
+		}
+	}
+}
+
+class Song {
+	String album;
+	String artist;
+	String title;
+	int track;
+
+	public Song(String album, String artist, String title, int track) {
+		this.album = album;
+		this.artist = artist;
+		this.title = title;
+		this.track = track;
+	}
+
+	double randomRank;
+	public void setRandomRank(double randomRank) {
+		this.randomRank = randomRank;
+	}
+}
