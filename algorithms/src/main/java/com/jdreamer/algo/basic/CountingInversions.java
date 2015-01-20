53305f0a1039662b20d112597bc7ddc5da556447
Hafizur Rahman <hafizur.rahman@kii.com>
2015-01-20 12:17:01 +0900

    Updated with coursera assignment

--- algorithms/src/main/java/com/jdreamer/algo/basic/CountingInversions.java
+++ algorithms/src/main/java/com/jdreamer/algo/basic/CountingInversions.java
@@ -1,17 +1,22 @@
 package com.jdreamer.algo.basic;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.util.Scanner;
+
 /**
  * http://www.geeksforgeeks.org/counting-inversions/
  */
 public class CountingInversions {
-    private static int countInversions(int[] a) {
+    private static long countInversions(int[] a) {
         int[] aux = new int[a.length];
 
         return mergeSort(a, aux, 0, a.length - 1);
     }
 
-    private static int mergeSort(int[] a, int[] aux, int left, int right) {
-        int count = 0;
+    private static long mergeSort(int[] a, int[] aux, int left, int right) {
+        long count = 0;
         if (right > left) {
             int mid = (left + right) / 2;
 
@@ -23,8 +28,8 @@ public class CountingInversions {
         return count;
     }
 
-    private static int merge(int[] a, int[] aux, int left, int mid, int right) {
-        int count = 0;
+    private static long merge(int[] a, int[] aux, int left, int mid, int right) {
+        long count = 0;
 
         int i = left;
         int j = mid;
@@ -58,11 +63,31 @@ public class CountingInversions {
         return count;
     }
 
-
     public static void main(String[] args) {
         int[] items = {1, 3, 5, 2, 4, 6};
 
         System.out.println(countInversions(items));
+
+        if (args.length > 0) {
+            String fileName = args[0];
+
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(new File(fileName));
+
+                int[] nums = new int [100000];
+                int i = 0;
+                while(scanner.hasNextInt()){
+                    nums[i++] = scanner.nextInt();
+                }
+
+                System.out.println(countInversions(nums));
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                IOUtils.closeQuietly(scanner);
+            }
+        }
     }
 
 }
